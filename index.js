5336f4f98b4f1ad923cafb66a81162b66a91de41
Sergey Romanov <xxsmotur@gmail.com>
2015-05-27 23:47:44 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,57 @@
+var CronJob = require('cron').CronJob;
+var request = require('request');
+var htmlparser = require("htmlparser2");
+var md5 = require('MD5');
+var chalk = require('chalk');
+
+
+
+var Wchanges = function() {
+
+};
+
+Wchanges.prototype.track = function(addr, time, fn) {
+     cron(addr, time, fn);
+};
+
+var get_page = function(addr) {
+    if(typeof addr === 'string') {
+        var md5_result = '';
+        request(addr, function(error, response, body){
+             if(!error && response.statusCode === 200){
+                 var result_str = '';
+                 var parser = new htmlparser.Parser({
+                    ontext: function(text){
+                        if(text.length > 1)
+                           result_str += text;
+                       },
+                 });
+                 parser.write(body);
+                 parser.end();
+                 if(md5_result === '')
+                   md5_result = md5(result_str);
+                 else{
+                    md5_current = md5(result_str);
+                    if(md5_current !== md5_result)
+                        console.log(addr + ' has changed');
+                 }
+
+
+
+             }
+        });
+    }
+};
+
+
+var cron = function(addr, time) {
+    console.log(chalk.green("Tracking of page " + addr + " is started"));
+    //Now is by default
+    new CronJob('* * * * * *', function() {
+        get_page(addr, time,fn);
+    }, null, true, 'America/Los_Angeles');
+
+};
+
+module.exports = Wchanges;
+
